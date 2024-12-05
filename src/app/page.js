eb51bfc0f4e77f213ec58337d424b93c8a172c2d
deveshbis <deveshbiswas.cse@gmail.com>
2024-12-05 10:04:47 +0600

    added product section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero/Hero";
+import Products from "@/components/Products/Products";
 
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Products/>
     </div>
   );
 }
